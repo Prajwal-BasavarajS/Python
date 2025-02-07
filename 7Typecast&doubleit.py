5736da63cd6f8a77418cef6eeca00e0fdc9bc9df
Prajwal Basavaraj <prajwalbasavaraj13@gmail.com>
2025-02-07 12:39:34 +0530

    first commit

---
+++ 7Typecast&doubleit.py
@@ -0,0 +1,12 @@
+# Given an input num as a string. You need to typecast into an integer and double it. 
+
+# Examples:
+
+# Input: num = "5"
+# Output: 10
+# Explanation: Typecast "5" to int and then double it 5 * 2 = 10
+
+a = input()
+b = int(a)
+c = b * 2
+print(f"{b} * 2 = {c}") 

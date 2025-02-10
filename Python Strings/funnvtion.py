b923d1104b2afd6ad7301300bd040c61b3900613
Prajwal Basavaraj <prajwalbasavaraj13@gmail.com>
2025-02-10 11:40:57 +0530

    first commit

---
+++ Python Strings/funnvtion.py
@@ -0,0 +1,15 @@
+def fun():
+    return 
+
+p = fun()
+print(p)  # Output: None
+
+for i in range(5):
+    pass  # Placeholder for future code
+
+
+def fun(n):
+    return [n*2, n*3]
+
+res = fun("hello")
+print(res)  

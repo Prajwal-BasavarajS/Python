c6dc4fdd6b554e29543d1aef2e6098a9e423fe2b
Prajwal Basavaraj <prajwalbasavaraj13@gmail.com>
2025-02-14 15:27:53 +0530

    first commit

--- Python Easy DSA/8_Next_Prime_no.py
+++ Python Easy DSA/8_Next_Prime_no.py
@@ -5,13 +5,13 @@
 # Input: n = 15
 # Output: 17
 # Explanation: 17 is next prime number.
-n = int(input())
+n = int(input())      #11
 
-n = n + 1
+n = n + 1             #12
 
 while True :
-    for i in range(2,n) :
-        if n % i == 0 :
+    for i in range(2,n) :    #(2,12) for loop till 11 => (2,3,4,5,6,7,8,9,10,11)
+        if n % i == 0 :      #
             break 
     else :
         print(n)
---
+++ Python Easy DSA/9_Fibonacci.py
@@ -0,0 +1,16 @@
+# Given an integer n. Write a program to find the nth Fibonacci number.
+
+# F(0)= 0, F(1)=1
+# The nth Fibonacci number is given by the forumla F(n) = F(n-1) + F(n-2). The first few fibonacci numbers are: 0 1 1 2 3 5. . . . 
+
+# Examples:
+
+# Input: n = 4
+# Output: 3
+# Explanation: In the series 0 1 1 2 3 5...... the fourth fibonacci number is 3.
+
+
+
+n = int (input())
+
+
